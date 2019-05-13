0b8aa0371217df0b6b774b2263a2810294ff7eae
Saul Nunez <saul.nunez99@gmail.com>
2019-05-12 19:20:41 -0600

    Atrapar excepciones

--- src/Shared/Stats/index.js
+++ src/Shared/Stats/index.js
@@ -1,34 +1,69 @@
 import { AsyncStorage } from "react-native";
 
-export async function setDifficulty(level){
-    await AsyncStorage.setItem('points', level);
+export async function setDifficulty(level) {
+    try {
+        await AsyncStorage.setItem('points', level);
+    } catch (error) {
+        console.error(error.message);
+    }
 }
 
-export async function updateStatistics(maxQuestionsAnswered){
-    await AsyncStorage.setItem('maxQuestionsAnswered', maxQuestionsAnswered);
+export async function updateStatistics(maxQuestionsAnswered) {
+    try {
+        await AsyncStorage.setItem('maxQuestionsAnswered', maxQuestionsAnswered);
+    } catch (error) {
+        console.error(error.message);
+    }
 }
 
-export default async function getDifficulty(){
-    return await AsyncStorage.getItem('points') || 0;
+export default async function getDifficulty() {
+    let points = 0;
+    try {
+        points = await AsyncStorage.getItem('points') || 0;
+    } catch (error) {
+        console.error(error.message);
+    }
+    return points;
 }
 
-export async function getStatistics(){
-    return await AsyncStorage.getItem('maxQuestionsAnswered') || 0;
+export async function getStatistics() {
+    let maxQuestionsAnswered = 0;
+    try {
+        maxQuestionsAnswered = await AsyncStorage.getItem('maxQuestionsAnswered') || 0;
+    } catch (error) {
+        console.error(error.message);
+    }
+    return maxQuestionsAnswered;
+
 }
 
-export async function purgeCurrentData(){
-    await AsyncStorage.removeItem('points');
-    await AsyncStorage.removeItem('maxQuestionsAnswered');
+export async function purgeCurrentData() {
+    try {
+        await AsyncStorage.removeItem('points');
+        await AsyncStorage.removeItem('maxQuestionsAnswered');
+    } catch (error) {
+        console.error(error.message);
+    }
+
 }
 
-export async function updateVolume({music, fx}){
-    await AsyncStorage.setItem('music', music);
-    await AsyncStorage.setItem('fx', fx);
+export async function updateVolume({ music, fx }) {
+    try {
+        await AsyncStorage.setItem('music', music);
+        await AsyncStorage.setItem('fx', fx);
+    } catch (error) {
+        console.error(error.message);
+    }
 }
 
-export async function getVolume(){
-    let fx = await AsyncStorage.getItem('volume') || 0;
-    let music = await AsyncStorage.getItem('fx') || 0;
-
-    return {fx, music};
+export async function getVolume() {
+    let fx = 0;
+    let music = 0;
+    try {
+        fx = await AsyncStorage.getItem('volume') || 0;
+        music = await AsyncStorage.getItem('fx') || 0;
+    } catch (error) {
+        console.error(error.message);
+    }
+    return { fx, music };
 }
