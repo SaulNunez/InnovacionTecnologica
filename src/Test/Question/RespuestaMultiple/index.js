dacc42707a52581044c09ef89d0decf6c2fb0a16
Saul Nunez <saul.nunez99@gmail.com>
2019-05-12 20:13:17 -0600

    Respuesta multiple pasada como un componente de react sin estado

--- src/Test/Question/RespuestaMultiple/index.js
+++ src/Test/Question/RespuestaMultiple/index.js
@@ -37,48 +37,29 @@ const styles = StyleSheet.create({
     }
 });
 
-export default class RespuestaMultiple extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { indexSelected: -1, question: props.question };
-    }
-
-    render() {
-        return (
-            <>
-                {this.props.question.answers.map((opcion, index, array) => {
-                    return (
-                        <Touchable key={index} onPress={() => {
-                            this.setState({ indexSelected: index });
-                            if (this.props.onOptionSelected) {
-                                const isRight = index === this.props.question.correctIndex;
-                                this.props.onOptionSelected(isRight);
-                            }
-                        }} 
-                        background={Touchable.Ripple('89CFF0')} 
-                        disabled={this.state.indexSelected !== -1}>
-                            <Text style={this.state.indexSelected === index ? styles.buttonSelected : styles.button}>{opcion}</Text>
-                        </Touchable>
-                    );
-                })}
-            </>
-        );
-    }
-
-    //Esto esta mal, si nos sale la pregunta dos veces seguida
-    //(una probabilidad de tal vez 1/24, 1/48 doble si consideraramos 50/50 que salga una pregunta teorica vs una practica)
-    //Pero probablemente solo lo provemos una o dos veces
-    //Y no permitira que el usuario eliga la sig. respuesta
-    static getDerivedStateFromProps(props, state){
-        if(props.question.question != state.question.question && state.indexSelected === -1){
-            return { indexSelected: -1 };
-        }
-
-        return null;
-    }
+const RespuestaMultiple = (props) => {
+    return(
+        <>
+        { props.question.answers.map((opcion, index, array) => {
+            return (
+                <Touchable key={index} onPress={() => {
+                    if (this.props.onOptionSelected) {
+                        const isRight = index === this.props.question.correctIndex;
+                        this.props.onOptionSelected(isRight);
+                    }
+                }} 
+                background={Touchable.Ripple('89CFF0')}                
+                disabled={props.indexSelected !== -1 || props.indexSelected !== null}>
+                    <Text style={props.indexSelected === index ? styles.buttonSelected : styles.button}>{opcion}</Text>
+                </Touchable>
+            );
+        })}
+        </>
+    );
 }
 
 RespuestaMultiple.propTypes = {
     question: PropTypes.object.isRequired,
     onOptionSelected: PropTypes.func.isRequired,
+    indexSelected: PropTypes.number.isRequired,
 };
